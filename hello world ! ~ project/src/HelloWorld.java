951de9b4a1db46e918a83958226f1b8379d2616f
APCS2nd <APCS2nd@MHS-LT9-11>
2019-08-21 12:17:00 -0700

    Complete Hello World project

---
+++ hello world ! ~ project/src/HelloWorld.java
@@ -0,0 +1,15 @@
+/* Robert Liu 
+   August 16th 2019
+   First program in java
+*/
+
+// prints simple message through the screen
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	
+	}
+
+}
