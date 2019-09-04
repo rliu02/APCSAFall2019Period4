3bc203b8c1ee831f909674c29125d463bbba38e0
APCS2nd <APCS2nd@MHS-LT9-11>
2019-09-04 12:05:42 -0700

    to cube

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,16 @@
+/* Robert Liu
+ * August 29th 2019
+ * Period 4
+ * Call math methods from Calculate Library 
+ * client code or runner code
+ */
+
+public class DoMath {
+
+	public static void main(String[] args) {
+		Calculate.square(12);
+		System.out.println(Calculate.square(12));
+
+	}
+
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,32 @@
+/* Robert Liu
+ * August 29 2019
+ * Period 4
+ * A group of methods that perform math calculations (library)
+ */
+
+public class Calculate {
+	// a call to square returns the square of a value passed
+	public static int square(int number) {
+		int answer=0;
+		answer = number*number;
+		return answer;
+		
+	}
+	
+	// a call to cube returns the cube of a value passed
+	public static int cube(int number) {
+		int answer=0;
+		answer = number*number*number;
+		return answer;
+		
+	}
+	
+	
+	// a call to cube returns the cube of a value passed
+	public static double average(double number1, number2) {
+		int answer=0;
+		answer = (number1+number2)/2;
+		return answer;		
+	}
+
+}
